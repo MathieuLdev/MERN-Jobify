61f0ad1154000bf11b28e025e3115b7fabfa6546
Mathieu <mathieu.linder.dev@gmail.com>
2022-05-16 12:45:00 +0200

    front routes

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import main from "../assets/images/main.svg";
 import Wrapper from "../assets/wrappers/LandingPage";
 import { Logo } from "../components";
@@ -19,7 +20,9 @@ const Landing = () => {
 						swag lumbersexual shoreditch schlitz. Next level 3 wolf moon brunch venmo
 						four dollar toast wayfarers.
 					</p>
-					<button className="btn btn-hero">Login/Register</button>
+					<Link to="/register" className="btn btn-hero">
+						Login/Register
+					</Link>
 				</div>
 				<img src={main} alt="job hunt" className="img main-img" />
 			</div>
