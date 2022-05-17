d88e0603ce80ae63a08fac3062670fc0efedc809
Mathieu <mathieu.linder.dev@gmail.com>
2022-05-17 07:25:41 +0200

    register page

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,5 +1,73 @@
+import { useState, useEffect } from "react";
+import { Logo, FormRow, Alert } from "../components";
+import Wrapper from "../assets/wrappers/RegisterPage";
+
+const initialState = {
+	name: "",
+	email: "",
+	password: "",
+	isMember: true,
+	showAlert: false,
+};
+
 const Register = () => {
-	return <h1>Register</h1>;
+	const [values, setValues] = useState(initialState);
+	// global state and useNavigate
+
+	const handleChange = (e) => {
+		console.log(e.target);
+	};
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+		console.log(e.target);
+	};
+
+	const toggleMember = () => {
+		setValues({ ...values, isMember: !values.isMember });
+	};
+
+	return (
+		<Wrapper className="full-page">
+			<form className="form" onSubmit={onSubmit}>
+				<Logo />
+				<h3>{values.isMember ? "Login" : "Register"}</h3>
+				{values.showAlert && <Alert />}
+				{/* name imput */}
+				{!values.isMember && (
+					<FormRow
+						type="text"
+						name="name"
+						value={values.name}
+						handleChange={handleChange}
+					/>
+				)}
+				{/* email imput */}
+				<FormRow
+					type="email"
+					name="email"
+					value={values.email}
+					handleChange={handleChange}
+				/>
+				{/* password imput */}
+				<FormRow
+					type="password"
+					name="password"
+					value={values.password}
+					handleChange={handleChange}
+				/>
+				<button type="submit" className="btn btn-block">
+					submit
+				</button>
+				<p>
+					{values.isMember ? "Not a member yet ?" : "Already a member ?"}
+					<button type="button" onClick={toggleMember} className="member-btn">
+						{values.isMember ? "Register" : "Login"}
+					</button>
+				</p>
+			</form>
+		</Wrapper>
+	);
 };
 
 export default Register;
