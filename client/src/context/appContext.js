d305a328059966a1a1228086a80e124ec37f7e07
Mathieu <mathieu.linder.dev@gmail.com>
2022-05-24 23:27:31 +0200

    profile page, axios interceptors and update user - client

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -9,6 +9,9 @@ import {
 	SETUP_USER_SUCCESS,
 	TOGGLE_SIDEBAR,
 	LOGOUT_USER,
+	UPDATE_USER_BEGIN,
+	UPDATE_USER_ERROR,
+	UPDATE_USER_SUCCESS,
 } from "./actions";
 
 const token = localStorage.getItem("token");
@@ -31,6 +34,35 @@ const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
 	const [state, dispatch] = useReducer(reducer, initialState);
+
+	// axios
+	// axios.defaults.headers.common["Authorization"] = `Bearer ${state.token}`;
+	const authFetch = axios.create({
+		baseURL: "/api/v1",
+	});
+	// request
+	authFetch.interceptors.request.use(
+		(config) => {
+			config.headers.common["Authorization"] = `Bearer ${state.token}`;
+			return config;
+		},
+		(error) => {
+			return Promise.reject(error);
+		},
+	);
+	// response
+	authFetch.interceptors.response.use(
+		(response) => {
+			return response;
+		},
+		(error) => {
+			// console.log(error.response);
+			if (error.response.status === 401) {
+				logoutUser();
+			}
+			return Promise.reject(error);
+		},
+	);
 
 	const displayAlert = () => {
 		dispatch({ type: DISPLAY_ALERT });
@@ -83,9 +115,34 @@ const AppProvider = ({ children }) => {
 		removeUserFromLocalStorage();
 	};
 
+	const updateUser = async (currentUser) => {
+		dispatch({ type: UPDATE_USER_BEGIN });
+		try {
+			const { data } = await authFetch.patch("/auth/updateUser", currentUser);
+			const { user, location, token } = data;
+			dispatch({ type: UPDATE_USER_SUCCESS, payload: { user, location, token } });
+			addUserToLocalStorage({ user, location, token });
+		} catch (error) {
+			if (error.response.status !== 401) {
+				dispatch({
+					type: UPDATE_USER_ERROR,
+					payload: { msg: error.response.data.msg },
+				});
+			}
+			clearAlert();
+		}
+	};
+
 	return (
 		<AppContext.Provider
-			value={{ ...state, displayAlert, setupUser, toggleSidebar, logoutUser }}
+			value={{
+				...state,
+				displayAlert,
+				setupUser,
+				toggleSidebar,
+				logoutUser,
+				updateUser,
+			}}
 		>
 			{children}
 		</AppContext.Provider>
