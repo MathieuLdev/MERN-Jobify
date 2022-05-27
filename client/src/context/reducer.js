190324a36b6816c0cbdd5b3c2efec9a278d7a7a1
Mathieu <mathieu.linder.dev@gmail.com>
2022-05-27 22:17:47 +0200

    pagination

--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -24,6 +24,7 @@ import {
 	SHOW_STATS_BEGIN,
 	SHOW_STATS_SUCCESS,
 	CLEAR_FILTERS,
+	CHANGE_PAGE,
 } from "./actions";
 import { initialState } from "./appContext";
 
@@ -128,6 +129,7 @@ const reducer = (state, action) => {
 	if (action.type === HANDLE_CHANGE) {
 		return {
 			...state,
+			page: 1,
 			[action.payload.name]: action.payload.value,
 		};
 	}
@@ -254,8 +256,15 @@ const reducer = (state, action) => {
 			...state,
 			search: "",
 			searchStatus: "all",
-			searchStatus: "all",
+			searchType: "all",
 			sort: "latest",
+		};
+	}
+
+	if (action.type === CHANGE_PAGE) {
+		return {
+			...state,
+			page: action.payload.page,
 		};
 	}
 
